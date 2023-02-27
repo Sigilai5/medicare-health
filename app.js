fe666f34c5643873d9d23cd1f657972a034a868e
Sigilai5 <briansigilai@gmail.com>
2023-02-27 03:05:57 +0300

    added crud oop

--- app.js
+++ app.js
@@ -34,7 +34,11 @@ app.post('/login', (req, res) => {
   } else {
     // Store user ID in session
     req.session.userId = user.id;
+    req.session.user_type=user.user_type;
+
     res.redirect('/dashboard');
+
+
   }
 });
 
@@ -45,12 +49,21 @@ app.get('/dashboard', (req, res) => {
     return;
   }
 
+   // Check if user is a doctor or a receptionist
+   if (req.session.user_type === 'doctor') {
+    res.redirect('/appointments');
+    } 
+
   // Load user data from JSON file
   const users = JSON.parse(fs.readFileSync('./Data/users.json'));
-  const patients = JSON.parse(fs.readFileSync('./Data/users.json'));
+  // Load patient data from JSON file
+  const patients = JSON.parse(fs.readFileSync('./Data/patients.json'));
 
   // Find user by ID
   const user = users.find(u => u.id === req.session.userId);
+
+  // Print username in console
+  console.log(`User ${user.username} is logged in`);
 
   // Serve the dashboard HTML page and replace [username] with the actual username
   res.render('dashboard', {username: user.username,patients:patients});
@@ -58,6 +71,39 @@ app.get('/dashboard', (req, res) => {
   // const dashboardPage = fs.readFileSync(__dirname + '/public/dashboard.html', 'utf8');
   // const renderedPage = dashboardPage.replace('[username]', user.username);
   // res.send(renderedPage);
+});
+
+app.get('/appointments', (req, res) => {
+  // Check if user is logged in
+  if (!req.session.userId) {
+
+    res.redirect('/login');
+    return;
+  }
+
+  // Check if user is a doctor or a receptionist
+  if (req.session.user_type === 'receptionist') {
+    res.redirect('/dashboard');
+    }
+
+
+
+
+  // Load user data from JSON file
+  const users = JSON.parse(fs.readFileSync('./Data/users.json'));
+
+   // Load patient data from JSON file
+   const patients = JSON.parse(fs.readFileSync('./Data/patients.json'));
+
+   // Load patients whose status is pending
+    const pendingPatients = patients.filter(patient => patient.status === 'pending');
+
+
+  // Find user by ID
+  const user = users.find(u => u.id === req.session.userId);
+
+  // Serve the appointments HTML page and replace [username] with the actual username
+  res.render('appointments', {username: user.username,patients:pendingPatients});  
 });
 
 
@@ -96,7 +142,7 @@ app.post('/register', (req, res) => {
 
     users.push(user);
     fs.writeFileSync('./Data/users.json', JSON.stringify(users));
-    res.send("Hooray")
+    res.send("Registered")
     
   }
 
@@ -104,6 +150,54 @@ app.post('/register', (req, res) => {
 
 
 // Patient routes
+
+//get all patients
+
+app.get('/getPatients', function (req, res) {
+  if (!req.session.userId) {
+    res.redirect('/login');
+    return;
+  }
+
+    fs.readFile('./Data/patients.json', (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error retrieving patient data');
+    } else {
+      const patients = JSON.parse(data);
+      res.render('dashboard', { patients });
+    }
+  });
+  
+});
+
+
+// get all records
+
+app.get('/history', function (req, res) {
+  if (!req.session.userId) {
+    res.redirect('/login');
+    return;
+  }
+
+    fs.readFile('./Data/records.json', (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error retrieving patient data');
+    } else {
+      const records = JSON.parse(data);
+      res.render('history', { records });
+    }
+  });
+
+});
+
+
+
+
+
+
+
 
 //get a specific patient
 
@@ -127,37 +221,121 @@ app.get('/getPatient',(req,res)=>{
 
 // add/update a patient
 
-app.post('/addPatient', (req,res)=>{
-  if (!req.session.userId) {
-    res.redirect('/login');
-    return;
-  }
+app.post('/addPatient', function (req, res) {
+  const name = req.body.name;
+  const age = req.body.age;
+  const gender = req.body.gender;
+  const contact = req.body.contact;
+  const address = req.body.address;
 
-  const { name, age, contact, gender,address } = req.body;
-  let patients = [];
-  patients = JSON.parse(fs.readFileSync('./Data/patients.json'));
+  const newPatient = {
+    name: name,
+    age: age,
+    gender: gender,
+    contact: contact,
+    address: address,
+    status:"pending"
+  };
 
-  const patient={
-    id: patients.length +1,
-    name:name,
-    age:age,
-    gender:gender,
-    contact:contact,
-    address:address
-  }
+  fs.readFile('./Data/patients.json', function (err, data) {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error: Could not read patients.json file');
+      return;
+    }
+
+    const patients = JSON.parse(data);
+    patients.push(newPatient);
 
 
-  if (req.session.user_type==='doctor') {
-    const doctor=Doctor(req.session.userId,req.session.username,req.session.user_type)
-    const response=doctor.addPatient(patient)
-    res.send(response);
-  }else if (req.session.user_type==='receptionist') {
-    const receptionist=Receiptionist(req.session.userId,req.session.username,req.session.user_type)
-    const response=receptionist.addPatient(patient)
-    res.send(response);
-  }
+    
+
+    fs.writeFile('./Data/patients.json', JSON.stringify(patients), function (err) {
+      if (err) {
+        console.log(err);
+        res.status(500).send('Error: Could not write to patients.json file');
+        return;
+      }
+
+      res.redirect('/');
+    });
+
+  });
+});
+
+//add a record to record.json file
+
+app.post('/addRecord', function (req, res) {
+  const name = req.body.name;
+  const contact = req.body.contact;
+  const condition = req.body.condition;
+  const description = req.body.description;
+  const prescription = req.body.prescription;
+
+
+  const newRecord = {
+    name: name,
+    contact: contact,
+    condition: condition,
+    description: description,
+    prescription: prescription,
+  };
+
+  fs.readFile('./Data/records.json', function (err, data) {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error: Could not read records.json file');
+      return;
+    }
+
+
+    const records = JSON.parse(data);
+    records.push(newRecord);
+
+    // change patient status to "seen" after adding a record
+    fs.readFile('./Data/patients.json', function (err, data) {
+      if (err) {
+        console.log(err);
+        res.status(500).send('Error: Could not read patients.json file');
+        return;
+      }
+
+      const patients = JSON.parse(data);
+      for (let i = 0; i < patients.length; i++) {
+        if (patients[i].contact === contact) {
+          patients[i].status = "seen";
+        }
+      }
+
+      fs.writeFile('./Data/patients.json', JSON.stringify(patients), function (err) {
+        if (err) {
+          console.log(err);
+          res.status(500).send('Error: Could not write to patients.json file');
+          return;
+        }
+      });
+
+
+    fs.writeFile('./Data/records.json', JSON.stringify(records), function (err) {
+      if (err) {
+        console.log(err);
+        res.status(500).send('Error: Could not write to records.json file');
+        return;
+      }
+
+      res.redirect('/');
+    }
+    );
+
+
+    
+  });
+});
 
 });
+
+
+
 
 //delete a patient 
 app.delete('/deletePatient', (req,res)=>{
@@ -183,6 +361,11 @@ app.delete('/deletePatient', (req,res)=>{
 
 app.get('/getRecord',(req,res)=>{
   if (!req.session.userId) {
+    res.redirect('/login');
+    return;
+  }
+
+  if (req.session.user_type==='receptionist') {
     res.redirect('/login');
     return;
   }
